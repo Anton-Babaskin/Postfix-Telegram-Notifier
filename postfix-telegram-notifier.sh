28f2f530de088c123238f78132bcd41ff9c3eca0
Anton Babaskin <me@fy-consulting.net>
2025-08-01 16:11:00 +0300

    postfix-telegram-notifier.sh

--- postfix-telegram-notifier.sh
+++ postfix-telegram-notifier.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -eu
 
 # ──────────────────────────────────────────────────────────────────────────────
 # Install.sh — ставит Postfix→Telegram tail-F-демон
